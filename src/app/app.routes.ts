cbbdcb351c7c843bad93146165893cea8178152b
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-06-18 17:04:01 -0300

    add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Cadastro } from './components/cadastro/cadastro';
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "Login", component: Login},
+    {path: "Home", component: Home},
+    {path: "Cadastrar", component: Cadastro}
+];
---
+++ src/app/components/cadastro/cadastro.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.css',
+})
+export class Cadastro {}
