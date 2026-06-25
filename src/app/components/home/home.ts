94ec63b462651895f3e670ffb9767feeeca4a3c6
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-06-25 01:12:18 -0300

    feat:(home.service and home.ts) captura dados da api

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HomeService } from '../../service/home.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,20 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit{
+
+  listaDeAnuncios: any[] = [];
+  constructor(private homeService: HomeService ) {}
+
+  ngOnInit(): void {
+    this.homeService.getAnuncios().subscribe({
+      next: (dados) => {
+        this.listaDeAnuncios = dados
+        console.log('Dados capturados', this.listaDeAnuncios)
+      },
+      error: (erro) => {
+        console.error('Erro ao conectar na API', erro);
+      }
+    })
+  }
+}
---
+++ src/app/service/home.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeService {
+
+  private API_URL = 'http://localhost:3001/anuncios';
+
+  constructor(private http: HttpClient) {
+    console.log(this.API_URL)
+  }
+
+  getAnuncios(): Observable<any[]> {
+    return this.http.get<any[]>(this.API_URL)
+  }
+  
+}
