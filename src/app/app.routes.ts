5d04ced3d4d010e8bfc1e8803b213fa5ae777a3e
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-06-26 16:03:49 -0300

    feat: adiciona component painel no app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Cadastro } from './components/cadastro/cadastro';
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Login } from './components/login/login';
+import { Painel } from './components/painel/painel';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: "login", component: Login},
     {path: "home", component: Home},
-    {path: "cadastrar", component: Cadastro}
+    {path: "cadastrar", component: Cadastro},
+    {path: "painelAnunciante", component: Painel}
 ];
