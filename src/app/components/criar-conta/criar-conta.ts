66f2c1a2d628c78d21cbbb51881476ee1773bd49
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-06-30 16:14:32 -0300

    style: alterar estruturação do html de fazer e criar login

--- src/app/components/criar-conta/criar-conta.ts
+++ src/app/components/criar-conta/criar-conta.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-criar-conta',
-  imports: [],
+   standalone: true,
+  imports: [RouterLink],
   templateUrl: './criar-conta.html',
   styleUrl: './criar-conta.css',
 })
-export class CriarConta {}
+export class CriarConta {
+  enviarDados() {}
+}
