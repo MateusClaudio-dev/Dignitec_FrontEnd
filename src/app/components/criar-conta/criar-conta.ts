82c4d4759cff2a4028fb6f694b4fafc31d16c2f0
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-06-30 19:59:04 -0300

    style: refatora estruturação html preparando para suporta ngModels

--- src/app/components/criar-conta/criar-conta.ts
+++ src/app/components/criar-conta/criar-conta.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-criar-conta',
    standalone: true,
-  imports: [RouterLink],
+  imports: [ FormsModule],
   templateUrl: './criar-conta.html',
   styleUrl: './criar-conta.css',
 })
-export class CriarConta {
+export class CriarConta implements OnInit {
   enviarDados() {}
+
+    ngOnInit(): void {
+      
+    }
+
+  dadosCadastro = {
+    nome: " ",
+    email: " ",
+    senha: " ",
+    confirmarSenha: " ",
+    tipo: " ",
+  }
 }
