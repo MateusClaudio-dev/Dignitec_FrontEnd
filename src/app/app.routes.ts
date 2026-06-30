b49709d23f2d5badeb58ecc4ce3de5106983429e
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-06-30 15:45:36 -0300

    feat: estruturar html e css da pagina de fazer e criar login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { Home } from './components/home/home';
 import { Login } from './components/login/login';
 import { Painel } from './components/painel/painel';
 import { EditarAnuncio } from './components/editar-anuncio/editar-anuncio';
+import { CriarConta } from './components/criar-conta/criar-conta';
 
 export const routes: Routes = [
     {path: '', component: Home},
+    {path: "criarConta", component: CriarConta},
     {path: "login", component: Login},
     {path: "home", component: Home},
     {path: "cadastrar", component: Cadastro},
---
+++ src/app/components/criar-conta/criar-conta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-conta',
+  imports: [],
+  templateUrl: './criar-conta.html',
+  styleUrl: './criar-conta.css',
+})
+export class CriarConta {}
